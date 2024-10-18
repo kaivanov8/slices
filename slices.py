7140b8ce3da9a02809bc95aadfac75b9cebd034e
kaivanov8 <kosiv6799@gmail.com>
2024-10-18 19:49:29 +0700

    Add files via upload

---
+++ slices.py
@@ -0,0 +1,8 @@
+# ПЗ строки и индексация строк
+
+example = 'пример'
+print(example[0]) # п
+print(example[-1]) # р
+print(example[3:6]) # мер
+print(example[::-1]) # ремирп
+print(example[1:6:2]) # рмр
